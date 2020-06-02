1ef8e90b6ccb40a742b6b1c1a57f2e5fffd2e21d
Juan Camilo Arias <jcamiloat@gmail.com>
2020-06-02 10:15:16 -0500

    Initial commit, everything working

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+const mongoUrl = config.MONGODB_URI;
+mongoose
+  .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info('Connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('Error connecting to DB: ', error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((error) => logger.error(error.message));
+});
+
+module.exports = app;
+
+// const PORT = 3003;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
---
+++ utils/logger.js
@@ -0,0 +1,7 @@
+const info = (msg) => console.log(msg);
+const error = (errorMsg) => console.error(errorMsg);
+
+module.exports = {
+  info,
+  error,
+};
