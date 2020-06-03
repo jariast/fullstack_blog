ff0d6b264fcdae3bff668f1b608070191d53c85d
Juan Camilo Arias <jcamiloat@gmail.com>
2020-06-02 19:39:07 -0500

    4.6 Must refactor the mostBlogs functions ASAP

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const dummy = () => {
   return 1;
 };
@@ -14,4 +16,23 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce(reducer);
 };
 
-module.exports = { dummy, totalLikes, favoriteBlog };
+// const mostBlogs = (blogs) => {
+//   let blogsByAuthor = _.countBy(blogs, 'author');
+//   blogsByAuthor = _.toPairs(blogsByAuthor);
+//   blogsByAuthor = blogsByAuthor.map((e) => ({
+//     author: e[0],
+//     numberOfBlogs: e[1],
+//   }));
+//   return blogsByAuthor;
+// };
+
+//TODO Refactor this!
+const mostBlogs = (blogs) => {
+  const blogsByAuthor = _.countBy(blogs, 'author');
+  const orderedAuthors = Object.entries(blogsByAuthor).sort(
+    (a, b) => b[1] - a[1]
+  );
+  return { author: orderedAuthors[0][0], blogs: orderedAuthors[0][1] };
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs };
